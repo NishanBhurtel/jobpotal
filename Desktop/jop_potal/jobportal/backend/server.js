dc137e2773f36b75ea9b440d0cbbe43e56af6529
Abishkardhenga <copywriterayan47@gmail.com>
2025-11-05 20:01:32 +0545

    ok

--- Desktop/jop_potal/jobportal/backend/server.js
+++ Desktop/jop_potal/jobportal/backend/server.js
@@ -1,3 +1,14 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+
+dotenv.config();
+
+const app = express();
+
 app.get('/', (req, res) => {
   res.send('🚀 Job Portal Backend Running Successfully!');
 });
